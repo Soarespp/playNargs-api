4eff59ced864afd5f04fa162aadae53ab6be321a
Pedro pp <pedropsoares@live.com>
2021-09-17 14:38:00 -0300

    change url atlas

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
+const URL_LOCAL = "mongodb://localhost/nodejs";
+const URL_ATLAS = "mongodb+srv://admin:admin@cluster0.z8gqt.mongodb.net/playnargs?retryWrites=true&w=majority"
+const URL_ATLAS2 = "mongodb+srv://admin:admin@cluster0.ci0zz.mongodb.net/nodejs?retryWrites=true&w=majority"
+
 class Connection {
     constructor() {
         this.dataBaseConnectionMongoDB();
     }
 
     dataBaseConnectionMongoDB() {
-        this.mongoDBConnection = mongoose.connect("mongodb://localhost/nodejs") //cria essa database automatico
+        this.mongoDBConnection = mongoose.connect(URL_ATLAS2) //cria essa database automatico
             .then(() => {
                 console.log("Conexão estabelicida com o MongoDB");
             })
